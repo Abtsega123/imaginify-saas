faf96c9358094cb5141826f68f1b36ec76155164
Abtsega123 <andyboringo1@gmail.com>
2024-05-07 17:05:02 +0530

    implement clerk

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs/server';
 
 export default authMiddleware()
+publicRoutes: ['/api/webhooks/clerk']
 
 export const config = {
   // The following matcher runs middleware on all routes
